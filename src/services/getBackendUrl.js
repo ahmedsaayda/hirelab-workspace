d7e2e50f6fa6a4235d78621c43c564655dec519c
ahmedsaayda <106405995+ahmedsaayda@users.noreply.github.com>
2026-03-14 21:23:53 +0000

    fix: update backend API URL to Render server

--- src/services/getBackendUrl.js
+++ src/services/getBackendUrl.js
@@ -1,4 +1,4 @@
 export const getBackendUrl = () =>
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:5155/api"
-    : process.env.NEXT_PUBLIC_BACKEND_URL;
+    ? process.env.NEXT_PUBLIC_BACKEND_URL
+    : "http://localhost:5155/api";
